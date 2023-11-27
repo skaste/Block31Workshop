a8d17e00a3932c8a9e75862f895651dcb8b0c690
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-27 14:32:32 -0600

    added new file for database and working on the nodemon and server.cjs

--- server.cjs
+++ server.cjs
@@ -1,4 +1,7 @@
 //create a const for express
+const express = require("express");
+
+const app = express();
 //create a port variable called PORT
 //Create four API endpoints and include the (req, res)
 //app.get() for all pets
