ddaf4d798192af1f7125e6fb34d66d98aa3c9ab9
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-27 14:34:44 -0600

    created PORT variable and assigned port 8080 and added a log to listen

--- server.cjs
+++ server.cjs
@@ -2,7 +2,12 @@
 const express = require("express");
 
 const app = express();
+
 //create a port variable called PORT
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log("Listening on port ${PORT}");
+});
 //Create four API endpoints and include the (req, res)
 //app.get() for all pets
 //app.get() for pets by name
