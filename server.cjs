b1e2bc14edbd269af85efed85dd76c8abddd1a25
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-27 14:13:30 -0600

    pseudocode for creating the server.cjs file

---
+++ server.cjs
@@ -0,0 +1,6 @@
+//create a const for express
+//create a port variable called PORT
+//Create four API endpoints and include the (req, res)
+//app.get() for all pets
+//app.get() for pets by name
+//app.get() for pets by owners names
