ffd8df812abfc54cda2330d1a240f297693775c6
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-11-27 21:45:00 -0600

    added a lowercase function so dont have to trust the person imputting the name if they use upper and lower case. Return the whole object if the name is found and put a 404 response if the dog is not in the list

--- server.cjs
+++ server.cjs
@@ -14,12 +14,24 @@ app.get("/api/v1/pets", (req, res) => {
 //app.get() for pets by name
 app.get("/api/v1/pets/:name", (req, res) => {
   const { name } = req.params;
-  res.send(`${name}`);
+
+  // Find the pet with the specified name in the pets array
+  const foundPet = pets.find(
+    (pet) => pet.name.toLowerCase() === name.toLowerCase()
+  );
+
+  if (foundPet) {
+    // If a matching pet is found, send back the pet object
+    res.json(foundPet);
+  } else {
+    // If no matching pet is found, send an appropriate response
+    res.status(404).json({ error: "Pet not found" });
+  }
 });
 //app.get() for pets by owners names
 app.get("/api/v1/pets/owner");
 //create a port variable called PORT
 const PORT = 8080;
 app.listen(PORT, () => {
-  console.log("Listening on port ${PORT}");
+  console.log(`Listening on port ${PORT}`);
 });
